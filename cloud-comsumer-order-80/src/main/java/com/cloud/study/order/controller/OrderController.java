0ee6c43e69ca71522fe4c621ac1004e7c119942c
v_liyichen <v_liyichen@baidu.com>
2020-07-20 17:36:01 +0800

    add comsumer

---
+++ cloud-comsumer-order-80/src/main/java/com/cloud/study/order/controller/OrderController.java
@@ -0,0 +1,30 @@
+package com.cloud.study.order.controller;
+
+
+import com.cloud.study.order.entities.CommonResult;
+import com.cloud.study.order.entities.Payment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class OrderController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public static final String PAYMENT_URL = "http://localhost:8001";
+
+    @GetMapping("/consumer/create")
+    public CommonResult<Payment> create(Payment payment) {
+        return restTemplate.postForObject(PAYMENT_URL+"payment/insert",payment,CommonResult.class);
+    }
+
+    @GetMapping("/consumer/get/{id}")
+    public CommonResult<Payment> getPatmentByID(@PathVariable("id")Long id) {
+        return restTemplate.getForObject(PAYMENT_URL + "/payment/get"+id,CommonResult.class);
+    }
+}
---
+++ cloud-comsumer-order-80/src/main/java/com/cloud/study/order/entities/Payment.java
@@ -0,0 +1,18 @@
+package com.cloud.study.order.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Payment implements Serializable {
+
+    private Long id;
+
+    private String serial;
+
+}
